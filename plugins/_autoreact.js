6192b215b41b2c932a7d02bcd772e97453925cb8
Mosyafik Jr <63044745+mosyafik@users.noreply.github.com>
2022-09-23 00:44:47 +0700

    Update _autoreact.js

--- plugins/_autoreact.js
+++ plugins/_autoreact.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn }) => {
     	}
     })	
 }
-handler.customPrefix = /(bile?k|ban?h|cum?|knt?l|y?|mmk|p|b(a|i)?c?(o|i)?(t|d)?|wibu|p(a)?nt(e)?k|pepe?k|m(ung)?k(i)?n|affa?h|br?o)/i
+handler.customPrefix = /(bile?k|ban?h|cum?|knt?l|y?|mmk|p|b(a|i)?c?(o|i)?(t|d)?|wibu|p(a)?nt(e)?k|pepe?k|m(ung)?k(i)?n|affa?h|br?o)/i|istri|oh|ywdah/apa/coba
 handler.command = new RegExp
 
 module.exports = handler
