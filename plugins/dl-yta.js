e0bf061bc8805e46ab5f809f55db822fd2bb7d6f
Mosyafik Jr <63044745+mosyafik@users.noreply.github.com>
2022-09-23 23:57:16 +0700

    Update dl-yta.js

--- plugins/dl-yta.js
+++ plugins/dl-yta.js
@@ -2,18 +2,18 @@ let limit = 30
 let fetch = require('node-fetch')
 const { servers, yta } = require('../lib/y2mate')
 let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
-  if (!args || !args[0]) throw `contoh:\n${usedPrefix + command} https://www.youtube.com/watch?v=yxDdj_G9uRY`
+  if (!args || !args[0]) throw `Contoh:\n${usedPrefix + command} https://www.youtube.com/watch?v=yxDdj_G9uRY`
   let chat = db.data.chats[m.chat]
   let server = (args[1] || servers[0]).toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF } = await yta(args[0], servers.includes(server) ? server : servers[0])
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-  m.reply(isLimit ? `Ukuran File: ${filesizeF}\nUkuran file diatas ${limit} MB, download sendiri: ${dl_link}` : data.wait)
+  m.reply(isLimit ? `Ukuran File: ${filesizeF}\nUkuran file diatas ${limit} MB, Download sendiri: ${dl_link}` : data.wait)
   if (!isLimit) conn.sendMedia(m.chat, dl_link, m, {
     asDocument: chat.useDocument, mimetype: 'audio/mp4', ptt: false, contextInfo: {
         externalAdReply: {
             showAdAttribution: true,
             mediaUrl: `${args}`,
-            title: 'Nih Kak Audionya', 
+            title: 'SELAMAT MENDENGARKAN🎶', 
             body: wm,
             description: wm,
             mediaType: 2,
